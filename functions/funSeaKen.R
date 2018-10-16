b5be78ee59cf550f29513878596f43a7c9def231
rmichie <michie.ryan@deq.state.or.us>
2018-10-16 15:39:32 -0700

    Update functions/funSeaKen.R
    -Fixed a bug that had qprob set at 0.05 instead of 0.5. This was used in the aggregation of samples into the monthly seasons. So 5th percentile instead of 50th!
    -Updated code so seaken dataframe will have a result of "Insufficient data for trend analysis" when it did not meet the minimum eight year requirement and when observations do not have an underlying sample regularity.

--- functions/funSeaKen.R
+++ functions/funSeaKen.R
@@ -17,7 +17,7 @@ run_seaKen <- function (inputData) {
       # specifiy current Station_ID
       tmp.one.station <- sea_ken_int$Station_ID[ii]
       tmp.data.raw <- inputData[inputData$Station_ID == tmp.one.station & 
-                               inputData$Analyte == parm,]
+                                  inputData$Analyte == parm,]
       sea_ken_int$analyte[ii] <- parm
       sea_ken_int$N[ii] <- length(tmp.data.raw$Result)
       if (!nrow(tmp.data.raw) > 1 | all(is.na(tmp.data.raw$Result))) {
@@ -41,16 +41,20 @@ run_seaKen <- function (inputData) {
                        type = "long",
                        time.format = "%Y-%m-%d %H:%M:%S")
       # Create time series from water quality data
-      tmp.ts <- suppressWarnings(tsMake(tmp.wq, focus = parm, qprob = 0.05, layer = c(0, 5))) #changed to median value per Helsel and Hirsch 2002; 11-14-2017
-      if (length(unique(year(tmp.data$date))) < 8) {
-        sea_ken_int$signif[ii] <- "Years<8"
+      tmp.ts <- suppressWarnings(tsMake(tmp.wq, focus = parm, qprob = 0.5, layer = c(0, 5))) #changed to median value per Helsel and Hirsch 2002; 11-14-2017
+      if (length(unique(year(tmp.data$date))) >= 8) {
+        # Need at least 8 years
+        sea_ken_int$signif[ii] <- "Insufficient data for trend analysis"
+        next
       }
       if (!length(tmp.ts) > 2 |
           start(tmp.ts)[1] == end(tmp.ts)[1] | 
           !any(1:frequency(tmp.ts) %in% cycle(tmp.ts)) |
-          frequency(tmp.ts) <= 1
-          #| !all(1:12 %in% cycle(tmp.ts))
-          ) next
+          frequency(tmp.ts) <= 1) {
+        # Observations do not have an underlying regularity
+        sea_ken_int$signif[ii] <- "Insufficient data for trend analysis"
+        next
+      }
       tmp.result <- seaKenPlus(tmp.ts)
       sea_ken_int$pvalue[ii] <- tmp.result$p.value
       sea_ken_int$slope[ii] <- tmp.result$sen.slope
@@ -66,8 +70,8 @@ run_seaKen <- function (inputData) {
     SeaKen$pvalue <- as.numeric(SeaKen$pvalue)
   }
   
-  SeaKen$signif <- ifelse(SeaKen$signif=="Years<8",
-                          "Need at least 8 years",
+  SeaKen$signif <- ifelse(SeaKen$signif=="Insufficient data for trend analysis",
+                          "Insufficient data for trend analysis",
                           ifelse(SeaKen$pvalue<=0.01, 
                                  "99% Significance Level",
                                  ifelse(SeaKen$pvalue<=0.05, 
@@ -82,7 +86,7 @@ run_seaKen <- function (inputData) {
 }
 
 seaKenPlus <- function (x, plot = FALSE, type = c("slope", "relative"), order = FALSE, 
-          pval = 0.05, mval = 0.5, pchs = c(19, 21), ...) 
+                        pval = 0.05, mval = 0.5, pchs = c(19, 21), ...) 
 {
   if (!is.numeric(x) && !is.matrix(x) && !is.data.frame(x)) 
     stop("'x' must be a vector, matrix, or data.frame")
