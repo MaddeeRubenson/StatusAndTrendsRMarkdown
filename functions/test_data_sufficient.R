33514089720658f1bec253699db3a363df0c500f
Erin <costello@pdx.edu>
2019-03-04 16:40:47 -0800

    changed object names to walker stout

--- functions/test_data_sufficient.R
+++ functions/test_data_sufficient.R
@@ -10,7 +10,7 @@ start.date = "2000-01-01"
 end.date = "2019-01-01"
 
 # Name <- "Walker Stout"
-# support_files_dir <- "//deqhq1/GISLIBRARY/Base_Data/Hydrography/Watershed_Boundaries/WBD_OR.gdb/WBD_OR.gdb/WBD_OR"
+
 basin_shp <- readOGR(dsn = "//deqhq1/WQNPS/Agriculture/Status_and_Trend_Analysis/SIAs/2019-SIA-Walker-Stout/GIS",
                           layer = 'WalkerStout_SIA', integer64="warn.loss", verbose = FALSE)
 
@@ -18,7 +18,7 @@ ws_stations_AWQMS <- get_stations_AWQMS(basin_shp)
 
 
 ws_data <- odeqstatusandtrends::GetData(parameters = c("Temperature", "Bacteria", "TSS", "DO", "TP", "pH"),
-                   stations_AWQMS = clack_stations_AWQMS,
+                   stations_AWQMS = ws_stations_AWQMS,
                    start.date = start.date,
                    end.date = end.date)
 ws_data <- CleanData(ws_data)
