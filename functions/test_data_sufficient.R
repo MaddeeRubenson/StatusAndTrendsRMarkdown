ef99b570e4c74dc3443fde512b34736604af4f33
Erin <costello@pdx.edu>
2019-03-07 11:20:44 -0800

    script for quick check of data availability

---
+++ functions/test_data_sufficient.R
@@ -0,0 +1,30 @@
+library(rgdal)
+library(RODBC)
+library(dplyr)
+library(odeqstatusandtrends)
+library(AWQMSdata)
+library(dataRetrieval)
+library(ggplot2)
+
+start.date = "2000-01-01"
+end.date = "2019-01-01"
+
+# Name <- "Walker Stout"
+
+basin_shp <- readOGR(dsn = "//deqhq1/WQNPS/Agriculture/Status_and_Trend_Analysis/SIAs/2019-SIA-Walker-Stout/GIS",
+                          layer = 'WalkerStout_SIA', integer64="warn.loss", verbose = FALSE)
+
+ws_stations_AWQMS <- get_stations_AWQMS(basin_shp)
+
+
+ws_data <- odeqstatusandtrends::GetData(parameters = c("Temperature", "Bacteria", "TSS", "DO", "TP", "pH"),
+                   stations_AWQMS = ws_stations_AWQMS,
+                   start.date = start.date,
+                   end.date = end.date)
+ws_data <- CleanData(ws_data)
+
+
+status <- status_stns(ws_data)
+
+trend <- trend_stns(ws_data)
+
